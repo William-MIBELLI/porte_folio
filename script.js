5c5ecb30e7d79332cb7c5dede344a4ce3ecb8e40
William MIBELLI <william.mibelli@gmail.com>
2022-07-27 23:55:47 +0200

    animation intro et divers tranform sur la page

---
+++ script.js
@@ -0,0 +1,33 @@
+let menu = document.querySelector('.menu')
+let item = document.querySelectorAll('.menu-item')
+let titre = document.querySelector('#titre')
+let sub = document.querySelector('#sub')
+let intro = document.querySelector('#intro')
+let btn = document.querySelector('#launcher')
+let main = document.querySelector('.main')
+let header = document.querySelector('.pres')
+
+
+let tl = new TimelineMax({paused: true})
+let tl2 = new TimelineMax({paused: true})
+
+
+tl
+.from(titre, 1 , {x:-200, opacity:0})
+.from(sub,1,{opacity:0},'<')
+.from(intro,1,{y:+100, opacity:0},'-=0.5')
+.from(btn, 1, {x:100, opacity:0},'-=0.3')
+
+tl2
+.from(menu, 0.4, {y:-120, opacity:0},'<')
+
+window.addEventListener('load', () => { tl.play() })
+
+btn.addEventListener('click', () => { 
+    header.style.marginTop = '0'
+    header.style.transition= 'all 0.6s ease-in-out'
+    main.style.display = 'block'
+    main.style.opacity= '1'
+    main.style.transition = 'display 2s ease'
+    tl2.play()
+})
